e3a315db96f4aa9fff235808b3bebe5b15ed80bc
Rodrigo <rodrigotecuatl@gmail.com>
2019-11-18 09:49:44 -0600

    Se modifico titulos y se agrego un hiperenlace

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+window.addEventListener('load', function() {
+  // Se agrego un hiperenlace al titulo
+  const btnNombre = document.querySelector('.link');
+  btnNombre.style.cursor = "pointer";
+  btnNombre.addEventListener('click', () => {
+    const main = window.location.href = "index.html";
+  })
+});
