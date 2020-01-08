aac6fb233b9f4b3410281156eaffe159319a120b
Rodrigo <rodrigotecuatl@gmail.com>
2020-01-07 22:45:39 -0600

    Se a creado algunas modificaciones en javaScript

--- js/main.js
+++ js/main.js
@@ -6,5 +6,21 @@ window.addEventListener('load', function() {
   btnNombre.style.cursor = "pointer";
   btnNombre.addEventListener('click', () => {
     const main = window.location.href = "index.html";
-  })
+  });
+
+  // información para obtener la ruta 
+  // https://cybmeta.com/obtener-la-url-de-la-pagina-actual-con-javascript-y-sus-componentes
+  
+  var nombredeRuta = window.location.pathname;
+  
+  if (nombredeRuta == '/index.html') {
+    console.log('Se encuentra en la página index.');
+  }
+  else if (nombredeRuta == '/portafolio.html') {
+    console.log('Se encuentra en la página portafolio.');
+  }
+  else if (nombredeRuta == '/contacto.html') {
+    console.log('Se encuentra en la página contacto.');
+  }
+  
 });
